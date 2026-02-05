24f5be991d02fdf1797e0331c329052cfae4196b
Hys-Lee <dltkdgus3275@gmail.com>
2026-02-05 19:38:55 +0900

    feat(BE-logging): 로그 시스템 제작

---
+++ src/main/java/com/PorTracker/PorTrackerBE/global/aspect/LogAspect.java
@@ -0,0 +1,28 @@
+package com.PorTracker.PorTrackerBE.global.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+
+    // service하위 패키지 모든 메서드 실행 시간 로깅
+    @Around("execution(* com.PorTracker.PorTrackerBE.service..*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executoinTime = System.currentTimeMillis() - start;
+
+        log.info("[Performance] {} executed in {}ms", joinPoint.getSignature().getName(),
+                executoinTime);
+
+        return proceed;
+    }
+}
