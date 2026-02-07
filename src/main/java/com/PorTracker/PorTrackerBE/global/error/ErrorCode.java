440c310a151e48f72f1ae9b68de1198ef8b3f904
Hys-Lee <dltkdgus3275@gmail.com>
2026-02-07 17:27:17 +0900

    feat(BE-google-api): redis에서 setnx 스핀락 적용
    추후 redisson기반 annotation화 할 것임.

--- src/main/java/com/PorTracker/PorTrackerBE/global/error/ErrorCode.java
+++ src/main/java/com/PorTracker/PorTrackerBE/global/error/ErrorCode.java
@@ -5,15 +5,18 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     // 공통
-    INVALID_INPUT_VALUE(400, "C001", " 올바르지 않은 입력값입니다."),
-    INTERNAL_SERVER_ERROR(500, "C002", " 서버 내부 오류가 발생했습니다."),
+    INVALID_INPUT_VALUE(400, "C001", " 올바르지 않은 입력값입니다."), INTERNAL_SERVER_ERROR(500, "C002",
+            " 서버 내부 오류가 발생했습니다."), TOO_MANY_REQUESTS(429, "C003",
+                    "잠시 후 다시 시도해주세요. 현재 요청이 처리 중입니다."),
 
     // 구글 시트
-    SHEET_NOT_FOUND(404, "S001", " 구글 시트를 찾을 수 없습니다."),
-    INVALID_ID_ACCESS_TOKEN(401, "S002", " 유효하지 않은 구글 엑세스 토큰입니다."),
+    SHEET_NOT_FOUND(404, "S001", " 구글 시트를 찾을 수 없습니다."), INVALID_ID_ACCESS_TOKEN(401, "S002",
+            " 유효하지 않은 구글 엑세스 토큰입니다."),
 
     // Supabase / DB
     DATS_SAVE_FAILED(500, "D001", " 데이터 저장 중 오류가 발생했습니다.");
+
+
 
     private final int status;
     private final String code;
--- src/main/java/com/PorTracker/PorTrackerBE/service/FinanceService.java
+++ src/main/java/com/PorTracker/PorTrackerBE/service/FinanceService.java
@@ -5,6 +5,8 @@ import com.PorTracker.PorTrackerBE.dto.AnonymizedStatsDto;
 import com.PorTracker.PorTrackerBE.dto.ComparisonDto;
 import com.PorTracker.PorTrackerBE.dto.GroupAverageResponse;
 import com.PorTracker.PorTrackerBE.dto.TransactionDto;
+import com.PorTracker.PorTrackerBE.global.error.BusinessException;
+import com.PorTracker.PorTrackerBE.global.error.ErrorCode;
 import com.PorTracker.PorTrackerBE.repository.SupabaseRepository;
 import com.PorTracker.PorTrackerBE.util.DateUtil;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -22,102 +24,110 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class FinanceService {
-    private final GoogleSheetService googleSheetService;
-    private final RedisTemplate<String, Object> redisTemplate;
-    private final StatisticsService statisticsService;
-    private final ObjectMapper objectMapper;
-    private final SupabaseRepository supabaseRepository;
+        private final GoogleSheetService googleSheetService;
+        private final RedisTemplate<String, Object> redisTemplate;
+        private final StatisticsService statisticsService;
+        private final ObjectMapper objectMapper;
+        private final SupabaseRepository supabaseRepository;
 
-    // 데이터 가져오고, 익명 통계 처리하기
-    public List<TransactionDto> getAndContributeStats(
-            String accessToken, String userId, String spreadsheetId, boolean refresh) {
+        // 데이터 가져오고, 익명 통계 처리하기
+        public List<TransactionDto> getAndContributeStats(String accessToken, String userId,
+                        String spreadsheetId, boolean refresh) {
 
-        // String cacheKey = "user:data:" + userId;
-        String cacheKey = "finance:data:" + userId + ":" + spreadsheetId; // 유저, 시트 별 고유하도록
+                // Locking - redis의 SETNX
+                String lockKey = "lock:finance:" + userId;
 
-        // refresh 수동 갱신에 대해 기존 캐시 삭제처리
-        if (refresh) {
-            log.info("cache forcefully remove req - key:{}", cacheKey);
-            redisTemplate.delete(cacheKey);
+                Boolean isLocked = redisTemplate.opsForValue().setIfAbsent(lockKey, "LOCKED", 10,
+                                TimeUnit.SECONDS);
+                if (Boolean.FALSE.equals(isLocked)) {
+                        throw new BusinessException(ErrorCode.TOO_MANY_REQUESTS);
+                }
+
+                try {
+
+                        // 데이터 가져오기
+                        String cacheKey = "finance:data:" + userId + ":" + spreadsheetId; // 유저, 시트
+                                                                                          // 별 고유하도록
+
+                        // refresh 수동 갱신에 대해 기존 캐시 삭제처리
+                        if (refresh) {
+                                log.info("cache forcefully remove req - key:{}", cacheKey);
+                                redisTemplate.delete(cacheKey);
+                        }
+
+                        // Redis 캐시 확인
+                        Object rawData = redisTemplate.opsForValue().get(cacheKey);
+                        List<TransactionDto> transactions;
+
+                        if (rawData != null) {
+                                log.info("Redis cache hit! - finance data");
+                                transactions = objectMapper.convertValue(rawData,
+                                                new TypeReference<List<TransactionDto>>() {});
+                        } else {
+                                log.info("redis cache miss! - using sheet api");
+                                try {
+                                        // 임시 범위
+                                        transactions = googleSheetService.getTransactions(
+                                                        accessToken, spreadsheetId, "A1:E100");
+
+                                        // redis 에 10분 저장
+                                        redisTemplate.opsForValue().set(cacheKey, transactions, 1,
+                                                        TimeUnit.HOURS);
+
+                                } catch (Exception e) {
+                                        log.error("data load failed", e);
+                                        return List.of();
+                                }
+                        }
+                        if (transactions.isEmpty()) {
+                                return transactions;
+                        }
+                        // 익명 통계 처리
+                        try {
+                                Map<String, Object> profile =
+                                                supabaseRepository.getUserProfile(userId);
+
+                                List<AnonymizedStatsDto> stats = statisticsService
+                                                .anonymized(userId, transactions, profile);
+                                log.info("created anonymous stats, data counts:{}", stats.size());
+
+                                // supabase에 저장 -> 비동기 처리
+                                statisticsService.contributeStats(stats);
+
+                        } catch (Exception e) {
+                                // 통계 처리 실패해도 데이터 넘기는 메인 로직은 중단 안되도록
+                                log.warn("Error At Stats Processing", e);
+                        }
+                        return transactions;
+
+                } finally {
+                        redisTemplate.delete(lockKey);
+                }
         }
 
-        // Redis 캐시 확인
-        Object rawData = redisTemplate.opsForValue().get(cacheKey);
-        List<TransactionDto> transactions;
+        public List<ComparisonDto> getComparison(String accessToken, String userId,
+                        String spreadsheetId, boolean refresh) {
+                List<TransactionDto> myData =
+                                getAndContributeStats(accessToken, userId, spreadsheetId, refresh);
 
-        if (rawData != null) {
-            log.info("Redis cache hit! - finance data");
-            transactions =
-                    objectMapper.convertValue(
-                            rawData, new TypeReference<List<TransactionDto>>() {});
-        } else {
-            log.info("redis cache miss! - using sheet api");
-            try {
-                // 임시 범위
-                transactions =
-                        googleSheetService.getTransactions(accessToken, spreadsheetId, "A1:E100");
+                Map<String, Long> myStats = myData.stream()
+                                .collect(Collectors.groupingBy(TransactionDto::category,
+                                                Collectors.summingLong(TransactionDto::amount)));
 
-                // redis 에 10분 저장
-                redisTemplate.opsForValue().set(cacheKey, transactions, 1, TimeUnit.HOURS);
+                Map<String, Object> profile = supabaseRepository.getUserProfile(userId);
+                String ageGroup = (String) profile.getOrDefault(ProfileSchema.AGE_GROUP,
+                                ProfileSchema.UNKNOWN);
+                String jobType = (String) profile.getOrDefault(ProfileSchema.JOB_TYPE,
+                                ProfileSchema.UNKNOWN);
+                String period = DateUtil.getCurrentPeriod();
 
-            } catch (Exception e) {
-                log.error("data load failed", e);
-                return List.of();
-            }
+                List<GroupAverageResponse> groupAvgs =
+                                supabaseRepository.getGroupAverages(ageGroup, jobType, period);
+
+                return groupAvgs.stream().map(avg -> {
+                        long myAcount = myStats.getOrDefault(avg.category(), 0L);
+                        return new ComparisonDto(avg.category(), myAcount, avg.avgAmount(),
+                                        myAcount - avg.avgAmount());
+                }).collect(Collectors.toList());
         }
-        if (transactions.isEmpty()) {
-            return transactions;
-        }
-        // 익명 통계 처리
-        try {
-            Map<String, Object> profile = supabaseRepository.getUserProfile(userId);
-
-            List<AnonymizedStatsDto> stats =
-                    statisticsService.anonymized(userId, transactions, profile);
-            log.info("created anonymous stats, data counts:{}", stats.size());
-
-            // supabase에 저장 -> 비동기 처리
-            statisticsService.contributeStats(stats);
-
-        } catch (Exception e) {
-            // 통계 처리 실패해도 데이터 넘기는 메인 로직은 중단 안되도록
-            log.warn("Error At Stats Processing", e);
-        }
-        return transactions;
-    }
-
-    public List<ComparisonDto> getComparison(
-            String accessToken, String userId, String spreadsheetId, boolean refresh) {
-        List<TransactionDto> myData =
-                getAndContributeStats(accessToken, userId, spreadsheetId, refresh);
-
-        Map<String, Long> myStats =
-                myData.stream()
-                        .collect(
-                                Collectors.groupingBy(
-                                        TransactionDto::category,
-                                        Collectors.summingLong(TransactionDto::amount)));
-
-        Map<String, Object> profile = supabaseRepository.getUserProfile(userId);
-        String ageGroup =
-                (String) profile.getOrDefault(ProfileSchema.AGE_GROUP, ProfileSchema.UNKNOWN);
-        String jobType =
-                (String) profile.getOrDefault(ProfileSchema.JOB_TYPE, ProfileSchema.UNKNOWN);
-        String period = DateUtil.getCurrentPeriod();
-
-        List<GroupAverageResponse> groupAvgs =
-                supabaseRepository.getGroupAverages(ageGroup, jobType, period);
-
-        return groupAvgs.stream()
-                .map(
-                        avg -> {
-                            long myAcount = myStats.getOrDefault(avg.category(), 0L);
-                            return new ComparisonDto(
-                                    avg.category(),
-                                    myAcount,
-                                    avg.avgAmount(),
-                                    myAcount - avg.avgAmount());
-                        })
-                .collect(Collectors.toList());
-    }
 }
