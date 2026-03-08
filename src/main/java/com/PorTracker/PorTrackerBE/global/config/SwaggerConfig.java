f28e3fa0f2e0379a4f2d2f0d8414d2648515bcb2
Hys-Lee <dltkdgus3275@gmail.com>
2026-03-08 18:32:38 +0900

    feat(BE-error): 스웨거에 에러 스키마 추가

--- src/main/java/com/PorTracker/PorTrackerBE/global/config/SwaggerConfig.java
+++ src/main/java/com/PorTracker/PorTrackerBE/global/config/SwaggerConfig.java
@@ -1,10 +1,19 @@
 package com.PorTracker.PorTrackerBE.global.config;
 
+import com.PorTracker.PorTrackerBE.global.error.ErrorResponse;
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Map;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,8 +26,12 @@ public class SwaggerConfig {
         // api 요청 시 인증 필요 항목 설정
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
 
+        // ResolvedSchema resolvedSchema =
+        //         ModelConverters.getInstance().readAllAsResolvedSchema(ErrorResponse.class);
+
         Components components =
                 new Components()
+                        // .addSchemas("ErrorResponse", resolvedSchema.schema)
                         .addSecuritySchemes(
                                 jwtSchemeName,
                                 new SecurityScheme()
@@ -35,5 +48,49 @@ public class SwaggerConfig {
                                 .version("v1.0.0"))
                 .addSecurityItem(securityRequirement)
                 .components(components);
+    }
+
+    @Bean
+    public OpenApiCustomizer customerGlobalHeaderOpenApiCustomiser() {
+        return openApi -> {
+            Map<String, Schema> schemas = ModelConverters.getInstance().read(ErrorResponse.class);
+            schemas.forEach((key, schema) -> openApi.getComponents().addSchemas(key, schema));
+            openApi.getPaths()
+                    .values()
+                    .forEach(
+                            pathItem ->
+                                    pathItem.readOperations()
+                                            .forEach(
+                                                    operation -> {
+                                                        ApiResponses responses =
+                                                                operation.getResponses();
+
+                                                        // 공통 에러 응답 정의
+                                                        ApiResponse errorResponse =
+                                                                new ApiResponse()
+                                                                        .description(
+                                                                                "에러 발생 (공통 구조)")
+                                                                        .content(
+                                                                                new Content()
+                                                                                        .addMediaType(
+                                                                                                "application/json",
+                                                                                                new MediaType()
+                                                                                                        .schema(
+                                                                                                                new Schema<>()
+                                                                                                                        .$ref(
+                                                                                                                                "#/components/schemas/ErrorResponse"))));
+
+                                                        // 모든 api에 공통 에러 응답 추가
+                                                        if (!responses.containsKey("400"))
+                                                            responses.addApiResponse(
+                                                                    "400", errorResponse);
+                                                        if (!responses.containsKey("401"))
+                                                            responses.addApiResponse(
+                                                                    "401", errorResponse);
+                                                        if (!responses.containsKey("500"))
+                                                            responses.addApiResponse(
+                                                                    "500", errorResponse);
+                                                    }));
+        };
     }
 }
--- src/main/java/com/PorTracker/PorTrackerBE/global/error/ErrorResponse.java
+++ src/main/java/com/PorTracker/PorTrackerBE/global/error/ErrorResponse.java
@@ -1,14 +1,23 @@
 package com.PorTracker.PorTrackerBE.global.error;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Schema(description = "공통 에러 응답 구조")
 public class ErrorResponse {
+    @Schema(description = "HTTP 상태 코드", example = "400")
     private final int status;
+
+    @Schema(description = "에러 코드", example = "C001")
     private final String code;
+
+    @Schema(description = "에러 메시지", example = "잘못된 입력값입니다.")
     private final String message;
+
+    @Schema(description = "에러 디테일", example = "어디에서 문제가 발생했습니다.")
     private final String detail;
 
     // ErrorResponse 생성하는 정적 팩토리 메서드 라고 함.
