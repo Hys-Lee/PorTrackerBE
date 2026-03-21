a9a42395e8e37c24273145d90d4023748cfec61b
Hys-Lee <dltkdgus3275@gmail.com>
2026-03-21 14:10:37 +0900

    feat(BE-deploy): 배포 체크 위한 redis 관련부분 추가 주석화

--- src/main/java/com/PorTracker/PorTrackerBE/global/aspect/DistributedLockAspect.java
+++ src/main/java/com/PorTracker/PorTrackerBE/global/aspect/DistributedLockAspect.java
@@ -1,16 +1,11 @@
 package com.PorTracker.PorTrackerBE.global.aspect;
 
 import com.PorTracker.PorTrackerBE.global.annotation.DistributedLock;
-import com.PorTracker.PorTrackerBE.global.error.BusinessException;
-import com.PorTracker.PorTrackerBE.global.error.ErrorCode;
-import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -18,31 +13,31 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class DistributedLockAspect {
-    private final RedissonClient redissonClient;
+    // private final RedissonClient redissonClient;
 
     @Around("@annotation(distributedLock)")
     public Object lock(ProceedingJoinPoint joinPoint, DistributedLock distributedLock)
             throws Throwable {
         String key = "lock:" + joinPoint.getArgs()[1]; // 임시
-        RLock lock = redissonClient.getLock(key);
+        // RLock lock = redissonClient.getLock(key);
 
         try {
-            boolean available =
-                    lock.tryLock(
-                            distributedLock.waitTime(),
-                            distributedLock.leaseTime(),
-                            TimeUnit.SECONDS);
+            // boolean available =
+            //         lock.tryLock(
+            //                 distributedLock.waitTime(),
+            //                 distributedLock.leaseTime(),
+            //                 TimeUnit.SECONDS);
 
-            if (!available) {
-                throw new BusinessException(ErrorCode.TOO_MANY_REQUESTS);
-            }
+            // if (!available) {
+            //     throw new BusinessException(ErrorCode.TOO_MANY_REQUESTS);
+            // }
 
             return joinPoint.proceed(); // 동작 메서드 실행
         } finally {
             // 내가 잡은 락에 대해 해제
-            if (lock.isHeldByCurrentThread()) {
-                lock.unlock();
-            }
+            // if (lock.isHeldByCurrentThread()) {
+            //     lock.unlock();
+            // }
         }
     }
 }
