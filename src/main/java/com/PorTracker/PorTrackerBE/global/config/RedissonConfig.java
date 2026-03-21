854f29822728e64cb78d0dfac8ef7d7c9c78149c
Hys-Lee <dltkdgus3275@gmail.com>
2026-03-21 17:47:13 +0900

    feat(BE-deploy): redis관련 설정 정상화

--- src/main/java/com/PorTracker/PorTrackerBE/global/config/RedissonConfig.java
+++ src/main/java/com/PorTracker/PorTrackerBE/global/config/RedissonConfig.java
@@ -1,5 +1,6 @@
 package com.PorTracker.PorTrackerBE.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -8,28 +9,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class RedissonConfig {
+    // @Value("${spring.data.redis.host}")
     @Value("${spring.data.redis.host}")
     private String host;
 
     @Value("${spring.data.redis.port}")
     private int port;
 
-    @Value("${spring.data.redis.pw : null}")
+    @Value("${spring.data.redis.password}")
     private String redissPw;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        if (redissPw != null) {
 
-            config.useSingleServer()
-                    .setAddress("redis://" + host + ":" + port)
-                    .setPassword(redissPw)
-                    .setSslEnableEndpointIdentification(true);
-        } else {
-            config.useSingleServer().setAddress("redis://" + host + ":" + port);
-        }
+        String redisUrl = "rediss://" + host + ":" + port;
+        // test
+        // log.info("redisUrl: {}, pw:{}", redisUrl, redissPw);
+        config.useSingleServer()
+                .setAddress(redisUrl)
+                .setPassword(redissPw)
+                .setSslEnableEndpointIdentification(false);
+
         return Redisson.create(config);
     }
 }
