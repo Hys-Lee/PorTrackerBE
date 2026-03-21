9120bf5270e87378e71839649c6563d730850545
Hys-Lee <dltkdgus3275@gmail.com>
2026-03-21 14:22:21 +0900

    feat(BE-deploy): redis 관련 부분 추가 주석

--- src/main/java/com/PorTracker/PorTrackerBE/global/config/RedissonConfig.java
+++ src/main/java/com/PorTracker/PorTrackerBE/global/config/RedissonConfig.java
@@ -1,35 +1,35 @@
-package com.PorTracker.PorTrackerBE.global.config;
+// package com.PorTracker.PorTrackerBE.global.config;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.redisson.Redisson;
+// import org.redisson.api.RedissonClient;
+// import org.redisson.config.Config;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RedissonConfig {
-    @Value("${spring.data.redis.host}")
-    private String host;
+// @Configuration
+// public class RedissonConfig {
+//     @Value("${spring.data.redis.host}")
+//     private String host;
 
-    @Value("${spring.data.redis.port}")
-    private int port;
+//     @Value("${spring.data.redis.port}")
+//     private int port;
 
-    @Value("${spring.data.redis.pw : null}")
-    private String redissPw;
+//     @Value("${spring.data.redis.pw : null}")
+//     private String redissPw;
 
-    @Bean
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        if (redissPw != null) {
+//     @Bean
+//     public RedissonClient redissonClient() {
+//         Config config = new Config();
+//         if (redissPw != null) {
 
-            config.useSingleServer()
-                    .setAddress("redis://" + host + ":" + port)
-                    .setPassword(redissPw)
-                    .setSslEnableEndpointIdentification(true);
-        } else {
-            config.useSingleServer().setAddress("redis://" + host + ":" + port);
-        }
-        return Redisson.create(config);
-    }
-}
+//             config.useSingleServer()
+//                     .setAddress("redis://" + host + ":" + port)
+//                     .setPassword(redissPw)
+//                     .setSslEnableEndpointIdentification(true);
+//         } else {
+//             config.useSingleServer().setAddress("redis://" + host + ":" + port);
+//         }
+//         return Redisson.create(config);
+//     }
+// }
